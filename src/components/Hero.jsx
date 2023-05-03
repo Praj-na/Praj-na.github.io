4dd0c88b32a78132cee5b9d74418274917b1215e
Praj-na <prajnapn36@gmail.com>
2023-05-03 12:25:05 +0300

    adding 3d computer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,13 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
-      </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Prajna</span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop responsive web applications,<br className='sm:block hidden'/> user interfaces and creative visuals.</p>
+        </div>
+        </div>
+
+        <ComputersCanvas />
     </section>
   )
 }
