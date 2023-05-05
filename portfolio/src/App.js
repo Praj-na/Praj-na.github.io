0109a3e62ffab9a23dba5a9e92283aafd808759d
Praj-na <prajnapn36@gmail.com>
2023-05-05 12:30:58 +0300

    change in tempate

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { NavBar } from './components';
+import { Hero, About, Projects, Technologies, Contact} from './container';
+import './App.css';
+
+const App = () => {
+  <div>
+    <NavBar />
+    <Hero />
+    <About />
+    <Projects /> 
+    <Technologies />
+    <Contact />
+  </div>
+}
+
+export default App;
